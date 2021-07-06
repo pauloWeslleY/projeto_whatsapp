9ab045da0b2b08294a684257368ea8b430229a99
pauloWeslleY13 <weslleyspfc13@outlook.com>
2021-07-06 01:48:13 -0300

    projeto wppClone aulaw17 a terminar

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -1,11 +1,13 @@
-class CameraController {
+export class CameraController {
    constructor(videoEl) {
       this._videoEl = videoEl;
       navigator.mediaDevices.getUserMedia({
          video: true
       }).then(stream => {
-         this._videoEl.src = URL.createObjectURL(stream);
+         let mediaStream = new MediaStream(stream);
+         this._videoEl.srcObject = mediaStream;
          this._videoEl.play();
+         // NOTE: this._videoEl.src = URL.createObjectURL(stream);
       }).catch(err => {
          console.error(err);
       });
--- src/util/Format.js
+++ src/util/Format.js
@@ -1,13 +1,9 @@
-class Format {
+export class Format {
 
    static getCamelCase(text) {
-
       let div = document.createElement('div');
-
       div.innerHTML = `<div data-${text}="id"></div>`;
-
       return Object.keys(div.firstChild.dataset)[0];
-
    }
 
    static toTime(duration) {
