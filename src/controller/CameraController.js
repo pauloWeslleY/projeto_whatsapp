e17369e691ba7f466d569ec65edea6e69d508164
pauloWeslleY <weslleyspfc13@outlook.com>
2021-07-06 20:48:36 -0300

    projeto wppClone aula18 finalizada

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -4,6 +4,7 @@ export class CameraController {
       navigator.mediaDevices.getUserMedia({
          video: true
       }).then(stream => {
+         this._stream = stream;
          let mediaStream = new MediaStream(stream);
          this._videoEl.srcObject = mediaStream;
          this._videoEl.play();
@@ -11,5 +12,20 @@ export class CameraController {
       }).catch(err => {
          console.error(err);
       });
+   }
+
+   stop() {
+      this._stream.getTracks().forEach((track) => {
+         track.stop();
+      });
+   }
+
+   takePicture(mimeType = 'image/png'){
+      let canvas = document.createElement('canvas');
+      canvas.setAttribute('height', this._videoEl.videoHeight);
+      canvas.setAttribute('width', this._videoEl.videoWidth);
+      let context = canvas.getContext('2d');
+      context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height);
+      return canvas.toDataURL(mimeType);
    }
 }
