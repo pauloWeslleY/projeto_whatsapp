be736136dae85e505bc4c0319a606985f0343739
pauloWeslleY13 <weslleyspfc13@outlook.com>
2021-07-16 01:37:46 -0300

    aula20 finalizada projetoWppClone

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,6 @@
 import {Format} from './../util/Format';
-import {CameraController} from './CameraController';
+import { CameraController } from './CameraController';
+import { DocumentPreviewController } from './DocumentPreviewController';
 
 export class WhatsAppController {
 	constructor() {
@@ -160,10 +161,10 @@ export class WhatsAppController {
 			this.el.containerTakePicture.show();
 			this.el.containerSendPicture.hide();
 		});
-		this.el.btnSendPicture.on('click', e => {
+		this.el.btnSendPicture.on('click', e=> {
 			console.log(this.el.pictureCamera.src);
 		});
-		this.el.btnAttachContact.on("click", e => {
+		this.el.btnAttachContact.on("click", e=> {
 			console.log("contact");
 			this.el.modalContacts.show();
 		});
@@ -173,6 +174,50 @@ export class WhatsAppController {
 			this.el.panelDocumentPreview.css({
 				height: "calc(100% - 120px",
 			});
+			this.el.inputDocument.click();
+		});
+		this.el.inputDocument.on('change', e => {
+			if (this.el.inputDocument.files.length) {
+				this.el.panelDocumentPreview.css({
+					height: '1%'
+				});
+				let file = this.el.inputDocument.files[0];
+				this._documentPreviewController = new DocumentPreviewController(file);
+				this._documentPreviewController.getPreviewData().then(result => {
+					this.el.imgPanelDocumentPreview.src = result.src;
+					this.el.infoPanelDocumentPreview.innerHTML = result.info;
+					this.el.imagePanelDocumentPreview.show();
+					this.el.filePanelDocumentPreview.hide();
+					this.el.panelDocumentPreview.css({
+						height: 'calc(100% - 120px'
+					});
+				}).catch(err => {
+					this.el.panelDocumentPreview.css({
+						height: 'calc(100% - 120px'
+					});
+					console.log(file.type);
+					switch (file.type) {
+						case 'application/vnd.ms.excel':
+						case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+							this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-xls';
+							break;
+						case 'application/vnd.ms.powerpoint':
+						case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+							this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-ppt'
+							break;
+						case 'application/msword':
+						case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+							this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-doc'
+							break;
+						default:
+							this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic'
+							break;
+					}
+					this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+					this.el.imagePanelDocumentPreview.hide();
+					this.el.filePanelDocumentPreview.show();
+				});
+			}
 		});
 		this.el.btnClosePanelDocumentPreview.on("click", e => {
 			this.closeAllMainPanel();
