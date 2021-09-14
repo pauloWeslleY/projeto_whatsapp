f7ac05cb5249dd5b771ac071e26a07c28cfe868c
pauloWeslleY13 <weslleyspfc13@outlook.com>
2021-09-13 23:22:15 -0300

    aula140-W29 projeto wpp clone

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -136,7 +136,11 @@ export class WhatsAppController {
 			}
 		});
 		this.el.btnSavePanelEditProfile.on("click", e => {
-			console.log(this.el.inputNamePanelEditProfile.innerHTML);
+			this.el.btnSavePanelEditProfile.disabled = true;
+			this._user.name = this.el.inputNamePanelEditProfile.innerHTML;
+			this._user.save().then(() => {
+				this.el.btnSavePanelEditProfile.disabled = false;
+			});
 		});
 		// NOTE: Quando esse formulário for enviado!
 		this.el.formPanelAddContact.on("submit", e => {
