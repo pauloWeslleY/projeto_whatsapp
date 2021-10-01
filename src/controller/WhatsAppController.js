950f2e34cb83fe49cbe9e4b37b7b9f8f8638db68
pauloWeslleY13 <weslleyspfc13@outlook.com>
2021-10-01 17:01:31 -0300

    aula144-W33 projeto wpp clone

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -112,6 +112,19 @@ export class WhatsAppController {
 					img.src = contact.photo;
 					img.show();
 				}
+				div.on('click', e => {
+					this.el.activeName.innerHTML = contact.name;
+					this.el.activeStatus.innerHTML = contact.status;
+					if (contact.photo) {
+						let img = this.el.activePhoto;
+						img.src = contact.photo;
+						img.show();
+					}
+					this.el.home.hide();
+					this.el.main.css({
+						display: 'flex'
+					});
+				});
 				this.el.contactsMessagesList.appendChild(div);
 			});
 		});
