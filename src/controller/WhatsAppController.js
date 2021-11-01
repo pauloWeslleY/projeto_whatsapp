0218432c9fa1e0df1080561526ed244fde5d5213
pauloWeslleY13 <weslleyspfc13@outlook.com>
2021-10-31 22:59:39 -0300

    aula151-W40 projeto wpp clone bug image

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -320,9 +320,8 @@ export class WhatsAppController {
 			this.el.inputPhoto.click();
 		});
 		this.el.inputPhoto.on('change', e => {
-			console.log(this.el.inputPhoto.files);
 			[...this.el.inputPhoto.files].forEach(file => {
-				console.log(file);
+				Message.sendImage(this._contactActive.chatId, this._user.email, file);
 			});
 		});
 		this.el.btnAttachCamera.on('click', e => {
--- src/model/Message.js
+++ src/model/Message.js
@@ -103,7 +103,7 @@ export class Message extends Model {
                                        </div>
                                     </div>
                               </div>
-                              <img src="#" class="_1JVSX message-photo" style="width: 100%; display:none">
+                              <img src="${this.content}" class="_1JVSX message-photo" style="width: 100%; display:none">
                               <div class="_1i3Za"></div>
                            </div>
                            <div class="message-container-legend">
@@ -127,6 +127,9 @@ export class Message extends Model {
                   </div>
                </div>
             `;
+            div.querySelector('.message-photo').on('load', e => {
+               console.log('load okay');
+            })
 				break;
 			case 'document':
 				div.innerHTML = `
@@ -273,7 +276,27 @@ export class Message extends Model {
       }
 		div.firstElementChild.classList.add(className);
 		return div;
-	}
+   }
+
+   static sendImage(chatId, from, file) {
+      return new Promise((s, f) => {
+         let uploadTask = Firebase.hd().ref(from).child(Date.now() + '_' + file.name).put(file);
+         uploadTask.on('state_changed', e =>{
+            console.info('upload', e);
+         }, err => {
+            console.error(err);
+         }, ()=>{
+            Message.send(
+               chatId,
+               from,
+               'image',
+               uploadTask.snapshot.downloadURL
+            ).then(() => {
+               s();
+            });
+         });
+      });
+   }
 
    static send(chatId, from, type, content) {
       return new Promise((s, f) => {
