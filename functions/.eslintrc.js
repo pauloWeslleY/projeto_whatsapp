81c694915831a15010c049825b8a579343d38032
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-02-25 14:03:08 -0300

    projeto wppClone aula160 não finalizada

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,15 +1,21 @@
-/* eslint-disable linebreak-style */
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
+   "root": true,
+   "env": {
+      es6: true,
+      node: true,
+   },
+   "extends": [
+      "eslint:recommended",
+      "google",
+   ],
+   "rules": {
+      "quotes": ["error", "double"],
+      "indent": ["error", 3],
+      // disable
+      "linebreak-style": "off",
+      "no-unused-vars": "off",
+      "no-tabs": "off",
+      "max-len": "off",
+      "prefer-const": "off",
+   },
 };
